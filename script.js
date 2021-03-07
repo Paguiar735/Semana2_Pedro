30025c9e026f43f6304e97467dba78c7ae9225e5
Paguiar735 <pvads.eng19@uea.edu.br>
2021-03-07 23:53:41 +0000

    Validation functions giving right results

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function Email_validation(email) {
-  if (email != "P"){
-    return "0";
-  }
-  else {
+function Validade(email) {
+  var allowed_emails = [
+    "edsfr.tur19@uea.edu.br",
+    "hgvn.eng19@uea.edu.br",
+    "jcss.tur18@uea.edu.br",
+    "pvads.eng19@uea.edu.br",
+    "sjvb.cic18@uea.edu.br",
+    "tsds.tur19@uea.edu.br", "000"]
+  if (allowed_emails.includes(email)){
     return "1";
   }
+  else {
+    return "0";
+  }
+}
+
+function Email_validation(email) {
+  var email = document.getElementById("myText").value;
+  document.getElementById("result").innerHTML = Validade(email);
 }
